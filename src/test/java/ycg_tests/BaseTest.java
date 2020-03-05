d801777171d6006ed6662fd1766b92247f18651f
markligeti <ligetimark.e@gmail.com>
2020-03-05 15:54:19 +0100

    implementation methods added for login tests

--- src/test/java/ycg_tests/BaseTest.java
+++ src/test/java/ycg_tests/BaseTest.java
@@ -1,6 +1,8 @@
 package ycg_tests;
 
 import Utility.Utility;
+import com.thoughtworks.gauge.AfterScenario;
+import com.thoughtworks.gauge.Step;
 import driver.Driver;
 import org.openqa.selenium.WebDriver;
 
@@ -8,9 +10,50 @@ public abstract class BaseTest {
     WebDriver driver = Driver.webDriver;
     String baseURL = Utility.getBaseURL();
 
-    // CREDENTIALS
     String USERNAME_USER = Utility.getUsername("user");
     String PASSWORD_USER = Utility.getPassword("user");
     String USERNAME_ADMIN = Utility.getUsername("admin");
     String PASSWORD_ADMIN = Utility.getPassword("admin");
+
+
+    @AfterScenario(tags = {"login, successful"})
+    public void logout() {
+        hoverOverProfileButton();
+        clickOnLogoutButton();
+    }
+
+    @Step("Hover over Profile button")
+    public void hoverOverProfileButton() {
+
+    }
+
+    @Step("Select Login/Registration button")
+    public void selectLoginRegButton() {
+
+    }
+
+    @Step("See the email address of <user_access>")
+    public void assertEmailAddressVisibleAndCorrect(String emailAddress) {
+
+    }
+
+    @Step("Click on Login button")
+    public void clickOnLoginButton() {
+
+    }
+
+    @Step("Click on Logout button")
+    public void clickOnLogoutButton() {
+
+    }
+
+    @Step("Enter <user_access> credentials")
+    public void enterCredentials(String userAccess) {
+
+    }
+
+    @Step("Check Login/Registration button")
+    public void assertLoginRegButtonAvailable() {
+
+    }
 }
