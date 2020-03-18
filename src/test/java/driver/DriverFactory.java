3333842869d832fe54ff7092370f779a69e73911
markligeti <ligetimark.e@gmail.com>
2020-03-18 13:55:00 +0100

    tests running in fullscreen

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -1,11 +1,11 @@
 package driver;
 
-import io.github.bonigarcia.wdm.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -29,7 +29,9 @@ public class DriverFactory {
                 WebDriverManager.chromedriver().setup();
 
 	            ChromeOptions options = new ChromeOptions();
-	            if ("Y".equalsIgnoreCase(System.getenv("HEADLESS"))) {
+                options.addArguments("start-maximized");
+
+                if ("Y".equalsIgnoreCase(System.getenv("HEADLESS"))) {
 	                options.addArguments("--headless");
 	                options.addArguments("--disable-gpu");
 	            }
